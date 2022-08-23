e54b912b69437e6590122205709e7ae6af4591bf
raphaelcostar <raphaelcosta.1@hotmail.com>
2022-08-23 18:52:13 -0300

    Implementando a classe Seller

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,6 +1,9 @@
 package application;
 
+import java.util.Date;
+
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Program {
 	
@@ -8,5 +11,9 @@ public class Program {
 		
 		Department dp = new Department(1, "Books");
 		System.out.println(dp);
+		
+		Seller seller = new Seller(21, "Bob", "bob@gmail.com", new Date(), 3000.0, dp);
+		System.out.println(seller);
+		
 	}
 }
