da5d9c816f1e073d7b78458fd5a1dc9aad688b2d
raphaelcostar <raphaelcosta.1@hotmail.com>
2022-08-29 15:23:04 -0300

    Implementando update

--- src/application/Program2.java
+++ src/application/Program2.java
@@ -12,10 +12,20 @@ public class Program2 {
 		DepartmentDao departmentDao = DaoFactory.createDepartmentDao();
 		
 		
-		System.out.println("--- TEST 1 - department insert");
+		
+		System.out.println("--- TESTE 1 - department insert");
 		Department dep = new Department(null, "Administrative");
-		departmentDao.insert(dep);
+		//departmentDao.insert(dep);
 		System.out.println("O id inserido foi: " + dep.getId());
+		System.out.println();
+		
+		
+		System.out.println("--- TESTE 2:  department update ---");
+		Department novo = new Department(9, "Food");
+		departmentDao.update(novo);
+		System.out.println("Update completed!");
+		
+		
 		
 	}
 }
