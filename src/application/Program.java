3ac8c1f26ae9796f66bf5a57c64e2cd1f9e5b09f
raphaelcostar <raphaelcosta.1@hotmail.com>
2022-08-29 10:25:06 -0300

    Implementando delete

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,7 +1,7 @@
 package application;
 
-import java.util.Date;
 import java.util.List;
+import java.util.Scanner;
 
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
@@ -11,6 +11,8 @@ import model.entities.Seller;
 public class Program {
 	
 	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
 		
 		SellerDao sellerDao = DaoFactory.createSellerDao();
 		
@@ -34,10 +36,29 @@ public class Program {
 			System.out.println(obj);
 		}
 		
+		/*
 		System.out.println("=== TEST 4: seller insert ===");
 		Seller newSeller = new Seller(null, "Raphael", "raphael@gmail.com", new Date(), 4000.0, department);
 		sellerDao.insert(newSeller);
 		System.out.println("Inserted Id" + newSeller.getId());
+		*/
+	
+		/*
+		System.out.println("=== TEST 5: seller update ===");
+		Seller updateSeller = new Seller(12,"Folukinha", "folukinha@gmail.com", new Date(), 4000.0, department);
+		sellerDao.update(updateSeller);
+		System.out.println(updateSeller);
+		*/
 		
+		System.out.println("=== TEST 6: seller delete ===");
+		System.out.print("Digite o Id do usário que você deseja deletar ");
+		Integer id = sc.nextInt();
+		Seller findById = sellerDao.findById(id);
+		System.out.println(findById);
+		sellerDao.deleteById(id);
+		
+		
+		}
 	}
-}
+	
+
--- src/model/dao/impl/SellerDaoJDBC.java
+++ src/model/dao/impl/SellerDaoJDBC.java
@@ -1,6 +1,7 @@
 package model.dao.impl;
 
 import java.sql.Connection;
+import java.sql.Date;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
@@ -9,6 +10,8 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+
+import com.mysql.cj.util.Base64Decoder;
 
 import db.DB;
 import db.DbException;
@@ -49,7 +52,7 @@ public class SellerDaoJDBC implements SellerDao {
 			if(rowsAffected > 0) {
 				ResultSet rs = st.getGeneratedKeys();
 				
-				if(rs.next()) {
+				while(rs.next()) {
 					int id = rs.getInt(1);
 					obj.setId(id);
 				}
@@ -70,13 +73,54 @@ public class SellerDaoJDBC implements SellerDao {
 
 	@Override
 	public void update(Seller obj) {
-		// TODO Auto-generated method stub
+		PreparedStatement st = null;
+		try {
+			st = conn.prepareStatement(
+					"UPDATE seller "
+					+ "SET name = ?, Email = ?, BirthDate = ?, BaseSalary = ?, DepartmentId = ? "
+					+ "WHERE Id = ?"
+				);
 		
+			st.setString(1, obj.getName());
+			st.setString(2, obj.getEmail());
+			st.setDate(3, new java.sql.Date(obj.getBirthDate().getTime()));
+			st.setDouble(4, obj.getBaseSalary());
+			st.setInt(5, obj.getDepartment().getId());
+			st.setInt(6, obj.getId());
+			
+			st.executeUpdate();
+		}
+		catch(SQLException e) {
+			System.out.println(e.getMessage());
+		}
+		finally {
+			DB.closeStatement(st);
+		}
+		
+			
+			
+			
 	}
 
 	@Override
 	public void deleteById(Integer id) {
-		// TODO Auto-generated method stub
+		PreparedStatement st = null;
+		
+		try {
+			st = conn.prepareStatement(
+					"DELETE FROM seller WHERE Id = ?");
+			
+			st.setInt(1, id);
+			
+			st.executeUpdate();
+		}
+		catch(SQLException e) {
+			throw new DbException(e.getMessage());
+		}
+		finally {
+			DB.closeStatement(st);
+		}
+		
 		
 	}
 
